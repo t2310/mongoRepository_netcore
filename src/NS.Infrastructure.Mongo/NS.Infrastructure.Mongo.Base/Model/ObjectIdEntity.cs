62bf8c211f9454cd0822a6c6a800e351989ac042
Vlad Tarasov <Administrator@Vlads-MacBook-Pro.local>
2017-08-13 12:56:35 +0300

    created mongo repository abstractions

---
+++ src/NS.Infrastructure.Mongo/NS.Infrastructure.Mongo.Base/Model/ObjectIdEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NS.Infrastructure.Mongo.Base.Model
+{
+    public abstract class ObjectIdEntity
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+}
---
+++ src/NS.Infrastructure.Mongo/NS.Infrastructure.Mongo.Base/Repository/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using NS.Infrastructure.Mongo.Base.Model;
+
+namespace NS.Infrastructure.Mongo.Base.Repository
+{
+    public abstract class Repository<T> where T : ObjectIdEntity
+    {
+        protected readonly IMongoCollection<T> MongoCollection;
+        protected Repository(string connectionString, string collection)
+        {
+            var mongoUrl = new MongoUrl(connectionString);
+            var client = new MongoClient(mongoUrl);
+            var mongoDatabase = client.GetDatabase(mongoUrl.DatabaseName);
+            MongoCollection = mongoDatabase.GetCollection<T>(collection);
+        }
+
+        public async Task<T> GetById(string id)
+        {
+            var result = await MongoCollection.Find(x => x.Id == id).FirstOrDefaultAsync().ConfigureAwait(false);
+            return result;
+        }
+
+        public async Task<T> Add(T item)
+        {
+            await MongoCollection.InsertOneAsync(item).ConfigureAwait(false);
+            return item;
+        }
+
+        public async Task<bool> DeleteById(string id)
+        {
+            var result = await MongoCollection.DeleteOneAsync(x => x.Id == id).ConfigureAwait(false);
+            return result.IsAcknowledged;
+        }
+    }
+}
